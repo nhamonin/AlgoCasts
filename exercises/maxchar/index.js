9dc19f7485651a5968b43a7dfdb9a6a09da30743
nhamonin <nazarii.hamonin@gmail.com>
2024-02-07 17:57:30 +0200

    Max chars

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const dictionary = str.split('').reduce((acc, curr) => {
+    acc[curr] = acc[curr] + 1 || 1;
+    return acc;
+  }, {});
+
+  let maxChar = '';
+  let maxCount = 0;
+
+  for (const [char, count] of Object.entries(dictionary)) {
+    if (count > maxCount) {
+      maxCount = count;
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
