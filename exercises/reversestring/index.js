9ae6d86d4db4a2e7a66cca2c2771341ccf5f7c89
nhamonin <nazarii.hamonin@gmail.com>
2024-02-07 12:34:02 +0200

    Reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,25 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// 1. Array.prototype.reverse()
+// function reverse(str) {
+//   return str.split('').reverse().join('');
+// }
+
+// 2. for of loop
+// function reverse(str) {
+//   let reversed = '';
+
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+
+//   return reversed;
+// }
+
+// 3. Array.prototype.reduce()
+function reverse(str) {
+  return str.split('').reduce((reversed, character) => character + reversed, '');
+}
 
 module.exports = reverse;
