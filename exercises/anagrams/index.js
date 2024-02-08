917b884bfb913462b649eb617c50ebfbf446c94a
nhamonin <nazarii.hamonin@gmail.com>
2024-02-08 22:33:55 +0200

    Anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -1,5 +1,5 @@
 // --- Directions
-// Check to see if two provided strings are anagrams of eachother.
+// Check to see if two provided strings are anagrams of each other.
 // One string is an anagram of another if it uses the same characters
 // in the same quantity. Only consider characters, not spaces
 // or punctuation.  Consider capital letters to be the same as lower case
@@ -8,6 +8,10 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  const refineStr = (str) => str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+
+  return refineStr(stringA) === refineStr(stringB);
+}
 
 module.exports = anagrams;
