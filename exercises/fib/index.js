d1064e9068ac3cf03454917e401f5b1ffbfe2367
nhamonin <nazarii.hamonin@gmail.com>
2024-02-10 20:07:27 +0200

    Fibonacci

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,41 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// 1. Iterative
+// function fib(n) {
+//   const result = [0, 1];
+
+//   for (i = 2; i <= n; i++) {
+//     result.push(result[i - 2] + result[i - 1]);
+//   }
+
+//   return result[n];
+// }
+
+// 2. Recursive
+// function fib(n) {
+//   return n < 2 ? n : fib(n - 1) + fib(n - 2);
+// }
+
+// 3. Recursive with memoization
+function memoize(fn) {
+  const cache = new Map();
+
+  return function (...args) {
+    const argsString = args.toString();
+    if (cache.get(argsString)) return cache.get(argsString);
+
+    const res = fn(...args);
+    cache.set(argsString, res);
+
+    return res;
+  };
+}
+
+function fib(n) {
+  return n < 2 ? n : fib(n - 1) + fib(n - 2);
+}
+
+fib = memoize(fib);
 
 module.exports = fib;
