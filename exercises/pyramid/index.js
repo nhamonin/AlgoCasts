97457a65f2b4de8fe843f489295f27b6f66d53af
nhamonin <nazarii.hamonin@gmail.com>
2024-02-10 16:04:08 +0200

    Pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,29 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// 1. Recursion
+// function pyramid(n, level = 1) {
+//   const lineLength = 2 * n - 1;
+//   const hashAmount = 2 * level - 1;
+//   const sideSpaces = (lineLength - hashAmount) / 2;
+
+//   console.log(' '.repeat(sideSpaces) + '#'.repeat(hashAmount) + ' '.repeat(sideSpaces));
+
+//   if (level >= n) return;
+
+//   pyramid(n, level + 1);
+// }
+
+// 2. Simple iteration over an array
+function pyramid(n) {
+  const lineLength = 2 * n - 1;
+
+  Array.from({ length: n }, (_, i) => i + 1).forEach((step) => {
+    const hashAmount = step * 2 - 1;
+    const sideSpaces = (lineLength - hashAmount) / 2;
+
+    console.log(' '.repeat(sideSpaces) + '#'.repeat(hashAmount) + ' '.repeat(sideSpaces));
+  });
+}
 
 module.exports = pyramid;
