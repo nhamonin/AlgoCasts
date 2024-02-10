c14741998d5f8dc087b8c95a64fe354b560436b4
nhamonin <nazarii.hamonin@gmail.com>
2024-02-10 18:40:27 +0200

    Spiral matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,48 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  const matrixArr = [];
+  let start = 0;
+  let end = n;
+
+  const numbers = Array.from({ length: Math.pow(n, 2) });
+
+  while (end <= n * n) {
+    matrixArr.push(numbers.slice(start, end));
+    start += n;
+    end += n;
+  }
+
+  let count = 1;
+  let startRow = 0;
+  let endRow = n - 1;
+  let startColumn = 0;
+  let endColumn = n - 1;
+
+  while (startRow <= endRow && startColumn <= endColumn) {
+    for (let i = startColumn; i <= endColumn; i++) {
+      matrixArr[startRow][i] = count++;
+    }
+    startRow++;
+
+    for (let i = startRow; i <= endRow; i++) {
+      matrixArr[i][endColumn] = count++;
+    }
+    endColumn--;
+
+    for (let i = endColumn; i >= startColumn; i--) {
+      matrixArr[endRow][i] = count++;
+    }
+    endRow--;
+
+    for (let i = endRow; i >= startRow; i--) {
+      matrixArr[i][startColumn] = count++;
+    }
+    startColumn++;
+  }
+
+  return matrixArr;
+}
 
 module.exports = matrix;
