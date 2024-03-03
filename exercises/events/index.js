06e7d2470457a3aa43195aa37f0756cec8748591
nhamonin <nazarii.hamonin@gmail.com>
2024-03-03 13:50:03 +0200

    Events

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -4,16 +4,36 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+  constructor() {
+    this.events = new Map();
+  }
+
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) {
+    const existingEventArray = this.events.get(eventName);
+
+    if (existingEventArray) {
+      existingEventArray.push(callback);
+    } else {
+      this.events.set(eventName, [callback]);
+    }
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    const callbacks = this.events.get(eventName) || [];
+
+    callbacks.forEach((callback) => {
+      callback();
+    });
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    this.events.delete(eventName);
+  }
 }
 
 module.exports = Events;
